df6335dca6c7b74635d48dcf0ac6c421b040d655
dast <dast@DastBook>
2015-06-16 18:22:28 +0200

    Přidíní osmé hodiny (Občas bývají :-) )

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
         <?php
             date_default_timezone_set("Europe/Prague");
             $intCas = intval(date("Hi"))+10;
-            $hodiny = array(0700,0842,0937,1032,1137,1227,1317);
+            $hodiny = array(0700,0842,0937,1032,1137,1227,1317,1407);
             $hodina=0;
 
-            for($i=6;$i>=0;$i--){
+            for($i=7;$i>=0;$i--){
                 if($intCas>$hodiny[$i]){
                     $hodina = $i+1;
                     break;
@@ -29,11 +29,9 @@
         <div class="karta" id="stred">
             <form action="ftp.php" method="post">
                 <input type="text" autofocus placeholder="Jméno" class="inputvkarte" pattern="[a-zěščřžýáíéóďťúů]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků." name="jmeno" required>
-                <input type="text" placeholder="Třída" class="inputvkarte" pattern="[a-z1-9]{2}" title="Jen malá písmena a čísla s délkou 2 znaků." name="trida" required><br>
-                <input type="text" placeholder="Počítač" class="inputvkarte" pattern="[1-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="pocitac" required>
-                <input type="text" placeholder="Učitel" class="inputvkarte" pattern="[a-z]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků a bez diakritiky." name="ucitel" required><br>
-                <input type="text" placeholder="Hodina" value=<?php echo ($hodina) ?> class="inputvkarte" pattern="[1-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="hodina" required>
-                <input type="text" placeholder="Učebna" class="inputvkarte" pattern="[a-z1-9]{2}" title="Jen malá písmena a čísla s délkou 2 znaků." name="ucebna" required><br>
+                <input type="text" placeholder="Počítač" class="inputvkarte" pattern="[1-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="pocitac" required><br>
+                <input type="text" placeholder="Učitel" class="inputvkarte" pattern="[a-z]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků a bez diakritiky." name="ucitel" required>
+                <input type="text" placeholder="Hodina" value=<?php echo ($hodina) ?> class="inputvkarte" pattern="[1-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="hodina" required><br>
                 <input type="submit" class="submitvkarte" value="Přihlásit">
             </form>
         </div>
