cec7ea80d05a229bf8707793e6b84e10906a5da0
Lukáš <Luk@Luke-book.local>
2015-06-22 12:40:06 +0200

    Odstranění ftp konstrukcí. Optimalizace kódu. Odstranění části php kódus index.php

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
         <meta charset="UTF-8">
         <link href="whale.png" rel="icon" type="image/png" />
         <title>SPŠE FTP Přihlašovač</title>
-        
-        
     </head>
+    
     <body>
         <?php 
             include("phpKnihovna.php");
@@ -17,34 +16,18 @@
                 $errorMessage = vsechno_funkce();
                 echo $errorMessage;
             }
+            $hodina = getHodina();
         ?>
-        <?php
-            date_default_timezone_set("Europe/Prague");
-            $intCas = intval(date("Hi"))+10;
-            $hodiny = array(0700,0842,0937,1032,1137,1227,1317,1407);
-            $hodina=0;
 
-            for($i=7;$i>=0;$i--){
-                if($intCas>$hodiny[$i]){
-                    $hodina = $i+1;
-                    break;
-                }
-            }
-        ?>
-        
-        
-        
         <div class="karta" id="stred">
             <form action="index.php" method="post">
                 <input type="text" autofocus placeholder="Jméno" class="inputvkarte" pattern="[a-zěščřžýáíéóďťúů]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků." name="jmeno" required>
                 <input type="text" placeholder="Počítač" class="inputvkarte" pattern="[0-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="pocitac" required><br>
                 <input type="text" placeholder="Učitel" class="inputvkarte" pattern="[a-z]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků a bez diakritiky." name="ucitel" required>
                 <input type="text" placeholder="Hodina" value=<?php echo ($hodina) ?> class="inputvkarte" pattern="[0-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="hodina" required><br>
+                <input type="text" placeholder="Report" class="inputvkarte" name="report"><br>
                 <input type="submit" class="submitvkarte" name="prihlasit" value="Přihlásit">
             </form>
         </div>
-        <?php
-        
-        ?>
     </body>
 </html>
--- phpKnihovna.php
+++ phpKnihovna.php
@@ -5,74 +5,75 @@
      */
     function get_client_ip() {
         $ipaddress = '';
-        if (getenv('HTTP_CLIENT_IP'))
+        if (getenv('HTTP_CLIENT_IP')){
             $ipaddress = getenv('HTTP_CLIENT_IP');
-        else if(getenv('HTTP_X_FORWARDED_FOR'))
+        }else if(getenv('HTTP_X_FORWARDED_FOR')){
             $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-        else if(getenv('HTTP_X_FORWARDED'))
+        }else if(getenv('HTTP_X_FORWARDED')){
             $ipaddress = getenv('HTTP_X_FORWARDED');
-        else if(getenv('HTTP_FORWARDED_FOR'))
+        }else if(getenv('HTTP_FORWARDED_FOR')){
             $ipaddress = getenv('HTTP_FORWARDED_FOR');
-        else if(getenv('HTTP_FORWARDED'))
+        }else if(getenv('HTTP_FORWARDED')){
            $ipaddress = getenv('HTTP_FORWARDED');
-        else if(getenv('REMOTE_ADDR'))
+        }else if(getenv('REMOTE_ADDR')){
             $ipaddress = getenv('REMOTE_ADDR');
-        else
+        }else{
             $ipaddress = 'UNKNOWN';
+        }
         return $ipaddress;
     }
     
     /**
-     * Funkce pro zobrazení karty s chybovou hláškou,
-     * @param type $info string je text hlášky
-     * @param type $die bool je informace o chtění smrti
+     * 
+     * @return int - číslo aktuální hodiny
      */
-    function show_card($info, $die=false) {
-        
-        $karta = $info;
-        
-        if($die){
-            die($karta);
-        }else{
-            echo ($karta);
+    function getHodina(){
+        date_default_timezone_set("Europe/Prague");
+        $intCas = intval(date("Hi"));
+        $hodiny = array(752,842,937,1032,1137,1227,1317,1407);
+        $hodina=0;
+        for($i=7;$i>=0;$i--){
+            if($intCas>($hodiny[$i]-10)){
+                $hodina = $i+1;
+                break;
+            }
         }
-        
+        return $hodina;
     }
     
     function vsechno_funkce() {
         date_default_timezone_set("Europe/Prague");
-        
+        //inicializace proměnných
         $jmeno = filter_input(INPUT_POST,"jmeno");
         $pocitac = filter_input(INPUT_POST,"pocitac");
         $ucitel = substr(filter_input(INPUT_POST,"ucitel"),0,3);
         $hodina = filter_input(INPUT_POST,"hodina");
+        $report = filter_input(INPUT_POST,"report");
         
+        //nalezení složky pro nahrání souborů
         $soubory = scandir("/home/".$ucitel);
+        $slozka = "";
         foreach($soubory as $s){
-            echo($s);
+            //if(!substr_compare($s,"q".date("md"),0) && !substr_compare($s,"h".$hodina,0)){
+            if(strpos($s,"q0617") !== false && strpos($s,"h3") !== false){
+                $slozka=$s; 
+                break;
+            }
         }
         
+        //vytvoření přihlašovacího soubou
+        if (!is_dir("prihlasovaky")) {
+            mkdir("prihlasovaky");         
+        }
+        $dir = "prihlasovaky/";
         $fileName = $pocitac.$jmeno.".txt";
-        $file = fopen($fileName, "w");
+        $file = fopen($dir.$fileName, "w");
+        fwrite($file,$report);
+        fclose($file);
 
-        $resource = ftp_connect("172.16.1.3");
-        if(!$resource){
-            return "Nelze se připojet k serveru";
-        }
-        
-        if(ftp_login($resource, "v01", "01")){
-           if(ftp_chdir($resource, "../".$ucitel."/".$slozka)){
-               $istrue = ftp_put($resource, $fileName, $fileName, FTP_ASCII);
-               if(!$istrue){
-                    return "Nepodařilo se přenest přihlašovaci soubor";
-               }
-               show_card("Přihlášení proběhlo úspěšně"); 
-           }else{
-               show_card("Nepodařilo se změnit složku na:<br>");
-               show_card("../".$ucitel."/".$slozka."<br>");
-           }
-        }else{
-            show_card("Nepodařilo se přihlásit uživatele");
+        //nahrání souboru
+        if(!copy($dir.$fileName, "/home/".$ucitel."/".$slozka."/".$fileName)){
+            return "Nepodařilo se zkopírovat soubor";
         }
 
         $link = mysqli_connect("localhost", "3c30", "3c30", "db_3c30");
@@ -80,15 +81,6 @@
             return "Nepodařilo se připojit k databázi.";
         }
         $ip = get_client_ip();
-        $result = mysqli_query($link,"SELECT * FROM ippc WHERE ip like '$ip'");
-        if(!$zaznam = mysqli_fetch_array($result)){
-            $istrue = mysqli_query($link,"INSERT INTO ippc VALUES(0,'$ip','$ucebna',$pocitac)");
-            if(!$istrue){
-                return "Nepodařilo se zaevidovat číslo počítače a učebnu.";
-            }
-            
-        }
     }
 
-?>
 
