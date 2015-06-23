6f4a30cbdbd1a7b7edcb25876656c019d5cc0512
dast <dast@DastBook>
2015-06-23 11:24:28 +0200

    Pridano zobrazeni cisla PC

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <div class="karta" id="stred">
             <form action="index.php" method="post">
                 <input type="text" autofocus placeholder="Jméno" class="inputvkarte" pattern="[a-zěščřžýáíéóďťúů]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků." name="jmeno" required>
-                <input type="text" placeholder="Počítač" class="inputvkarte" pattern="[0-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="pocitac" required><br>
+                <input type="text" placeholder="Počítač" class="inputvkarte" pattern="[0-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="pocitac" required value=<?php getCisloPc(get_client_ip()) ?>><br>
                 <input type="text" placeholder="Učitel" class="inputvkarte" pattern="[a-z]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků a bez diakritiky." name="ucitel" required>
                 <input type="text" placeholder="Hodina" value=<?php echo ($hodina) ?> class="inputvkarte" pattern="[0-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="hodina" required><br>
                 <input type="text" placeholder="Report" class="inputvkarte" name="report"><br>
