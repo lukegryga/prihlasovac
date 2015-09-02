0cc5fac64ab92fadc784adb55c8786cb21c2237b
dastd <dastd@DESKTOP-A9DG0HO.local>
2015-09-02 12:07:31 +0200

    desing

--- index.php
+++ index.php
@@ -23,10 +23,15 @@
 
         <div class="karta" id="stred">
             <form action="index.php" method="post">
+                <input type="text" class="inputvkarte" id="nadpis_input" value="Jméno" readonly="true">
+                <input type="text" class="inputvkarte" id="nadpis_input" value="Počítač" readonly="true"><br>
                 <input type="text" autofocus placeholder="Jméno" class="inputvkarte" pattern="[a-zěščřžýáíéóďťúů]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků." name="jmeno" required>
                 <input type="text" placeholder="Počítač" class="inputvkarte" pattern="[0-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="pocitac" required value="<?php echo getCisloPc(get_client_ip()) ?>" ><br>
+                <input type="text" class="inputvkarte" id="nadpis_input" value="Jméno" readonly="true">
+                <input type="text" class="inputvkarte" id="nadpis_input" value="Počítač" readonly="true"><br>
                 <input type="text" placeholder="Učitel" class="inputvkarte" pattern="[a-z]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků a bez diakritiky." name="ucitel" required>
                 <input type="text" placeholder="Hodina" value=<?php echo ($hodina) ?> class="inputvkarte" pattern="[0-9]{1}" title="Jen čísla s délkou 2 znaků." name="hodina" required><br>
+                <input type="text" class="inputvkarte" id="nadpis_input" value="Jméno" readonly="true"><br>
                 <input type="text" placeholder="Report" class="inputvkarte" name="report"><br>
                 <input type="submit" class="submitvkarte" name="prihlasit" value="Přihlásit">
             </form>
