6e339453565753f90a0cbff449876e237bf0cdd5
dastd <dastd@DESKTOP-A9DG0HO.local>
2015-09-03 12:49:12 +0200

    Hodne uprav a nadpis

--- index.php
+++ index.php
@@ -20,15 +20,18 @@
             }
             $hodina = getHodina();
         ?>
-
+        <div class="karta" id="stred">
+            <h1> SPŠE Přihlašovač</h1>
+        </div>
+        
         <div class="karta" id="stred">
             <form action="index.php" method="post">
                 <input type="text" class="inputvkarte" id="nadpis_input" value="Jméno" readonly="true">
                 <input type="text" class="inputvkarte" id="nadpis_input" value="Počítač" readonly="true"><br>
                 <input type="text" autofocus placeholder="Jméno" class="inputvkarte" pattern="[a-zěščřžýáíéóďťúů]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků." value=<?php echo"\""; echo getJmeno(); echo"\""; ?> name="jmeno" required>
                 <input type="text" placeholder="Počítač" class="inputvkarte" pattern="[0-9]{1,2}" title="Jen čísla s délkou 2 znaků." name="pocitac" required value="<?php echo getCisloPc(get_client_ip()) ?>" ><br>
-                <input type="text" class="inputvkarte" id="nadpis_input" value="Jméno" readonly="true">
-                <input type="text" class="inputvkarte" id="nadpis_input" value="Počítač" readonly="true"><br>
+                <input type="text" class="inputvkarte" id="nadpis_input" value="Učitel" readonly="true">
+                <input type="text" class="inputvkarte" id="nadpis_input" value="Hodina" readonly="true"><br>
                 <select>
                     <?php
                     $seznamUcitelu = getSeznamUcitelu();
@@ -43,7 +46,7 @@
                 </select>
                 <!--<input type="text" placeholder="Učitel" class="inputvkarte" pattern="[a-z]{1,12}" title="Jen malá písmena s maximální délkou 12 znaků a bez diakritiky." name="ucitel" required>-->
                 <input type="text" placeholder="Hodina" value=<?php echo ($hodina) ?> class="inputvkarte" pattern="[0-9]{1}" title="Jen čísla s délkou 2 znaků." name="hodina" required><br>
-                <input type="text" class="inputvkarte" id="nadpis_input" value="Jméno" readonly="true"><br>
+                <input type="text" class="inputvkarte" id="nadpis_input" value="Report" readonly="true"><br>
                 <input type="text" placeholder="Report" class="inputvkarte" name="report"><br>
                 <input type="submit" class="submitvkarte" name="prihlasit" value="Přihlásit">
             </form>
